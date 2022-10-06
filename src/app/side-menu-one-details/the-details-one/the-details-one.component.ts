a87d257b685ce3d54aaa72f7b1c2cf2f587bbb40
Timothy Loughhead <timothy.loughhead@playtimesolutions.com.au>
2022-10-06 17:01:22 +1100

    Accordion opened animation

--- src/app/side-menu-one-details/the-details-one/the-details-one.component.ts
+++ src/app/side-menu-one-details/the-details-one/the-details-one.component.ts
@@ -1,16 +1,27 @@
 import { Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap';
+import { trigger, state, style, animate, transition, } from '@angular/animations';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-the-details-one',
   templateUrl: './the-details-one.component.html',
   styleUrls: ['./the-details-one.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('true', style({ backgroundColor: '#bcc3cb' })),
+      state('false', style({})),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
+    ]),
+  ],
 })
 export class TheDetailsOneComponent implements OnInit {
 
   @Input() menuItemSelected: { menuItemId: string } = { menuItemId: '' };
   @ViewChild('acc') accordionComponent: NgbAccordion | undefined;
   selectedMenuItem: string = '';
+  isSelected = '';
 
   constructor() { }
 
@@ -20,8 +31,23 @@ export class TheDetailsOneComponent implements OnInit {
   ngOnChanges(changes: SimpleChanges) {
     for (const propName in changes) {
       if (propName === 'menuItemSelected' && changes[propName].currentValue.menuItemId !== '') {
+        this.selectedMenuItem = changes[propName].currentValue.menuItemId;
         this.handleMenuItemSelected(changes[propName].currentValue.menuItemId);
+
+        timer(1500).subscribe(() => this.selectedMenuItem = '');
       }
+    }
+  }
+
+  isSelectedMenuItem(selectedMenuItemId: string): string {
+    return (this.selectedMenuItem === selectedMenuItemId).toString();
+  }
+
+  toggle() {
+    if (this.isSelected === 'selected') {
+      this.isSelected = '';
+    } else {
+      this.isSelected = 'selected';
     }
   }
 
