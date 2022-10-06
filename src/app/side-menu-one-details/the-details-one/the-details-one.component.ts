d9922023f76139204a07adf48222c945c924b32e
Timothy Loughhead <timothy.loughhead@playtimesolutions.com.au>
2022-10-06 14:55:00 +1100

    Scroll on menu item click

--- src/app/side-menu-one-details/the-details-one/the-details-one.component.ts
+++ src/app/side-menu-one-details/the-details-one/the-details-one.component.ts
@@ -20,7 +20,26 @@ export class TheDetailsOneComponent implements OnInit {
   ngOnChanges(changes: SimpleChanges) {
     for (const propName in changes) {
       if (propName === 'menuItemSelected' && changes[propName].currentValue.menuItemId !== '') {
-        this.accordionComponent?.toggle(changes[propName].currentValue.menuItemId);
+        this.handleMenuItemSelected(changes[propName].currentValue.menuItemId);
+      }
+    }
+  }
+
+  /**
+   * When a menu item has been selected
+   * - Open the accordion panel with the matching id
+   * - Scroll to the selected panel
+   * - Flash the header (or something...)
+   */
+  handleMenuItemSelected(menuItemSelected: string): void {
+    this.accordionComponent?.expand(menuItemSelected);
+    const menuElement = document.getElementById(menuItemSelected);
+
+    if (menuElement) {
+      const topPos = menuElement.offsetTop - 40;
+      const theContainer = document.getElementById('theRoute');
+      if (theContainer) {
+        theContainer.scrollTo({ top: topPos, behavior: 'smooth' });
       }
     }
   }
